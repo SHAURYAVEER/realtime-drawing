e1e792327c4faac3cbdbca40d423edf1d9d6df39
SHAURYAVEER <63491887+SHAURYAVEER@users.noreply.github.com>
2021-10-13 20:37:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+noseX=0;
+noseY=0;
+
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+
+
 function setup()
 {
     video = createCapture(VIDEO);
@@ -17,8 +26,29 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        noseX= results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose = " + noseX + "noseY = " + noseY);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        
+         console.log("leftwrist = " + leftWristX + "rightWrist" + rightWristX + "difference" + difference);
     }
 }
+
+function draw()
+{
+    background('#969A97');
+    document.getElementById("square_side").innerHTML = "Width and height of the square will be = " + difference + "px";
+    fill('#F90012');
+   stroke('#F90012');
+   square(noseX, noseY, difference);
+
+}
+
+
+
 
 
 
